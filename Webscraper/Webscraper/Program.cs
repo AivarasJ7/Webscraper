501ef037f27a0adcc3f52c2364e083d7c2f72f9b
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-10-01 23:20:27 +0300

    weather

--- Webscraper/Webscraper/Program.cs
+++ Webscraper/Webscraper/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Webscraper
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
 
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // Send get request to gismeteo.lt
+            String url = "https://www.gismeteo.lt";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // Get the temparature
+
+            // Get the conditions
+
+            // Get the location
         }
     }
 }
